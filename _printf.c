f47ffa0fe1e9f4ad11c5678d66b0bb81aab152f5
fatimazaarate <fatima.zaarate@gmail.com>
2023-08-21 16:39:56 +0100

    modifications

--- _printf.c
+++ _printf.c
@@ -1,65 +1,66 @@
-#include " main.h" 
+#include " main.h"
+
 /**
 * _printf - create our own printf function
 * @format: a character string
-* 
+*
 * Return: the number of characters printed
 */
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int index = 0;
-        char c;
-        int d, leng;
-        char *str;
+	va_list args;
+	int index = 0;
+	char c;
+	int d, leng;
+	char *str;
 
-        va_start(args, format);
+	va_start(args, format);
 
-        if (format == NULL)
-                return(-1);
+	if (format == NULL)
+		return  (-1);
 
-        while(*format)
-        {
-                if (*format == '%')
-                {
-                    format++;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
 
-                    if (*format == '\0')
-                                break;
+		if (*format == '\0')
+			break;
 
-                else if ( *format == 'c')
-                {
-                        c = va_arg(args, int);
-                        write(1, &c, 1);
-                        index++;
-                }
-                else if (*format == 's')
-                {
-                        str = va_arg(args, char *);
-                        write(1, str, strlen(str));
-                        index += strlen(str);
-                }
-                else if ( *format == 'd' || *format == 'i')
-                {
-                        d = va_arg(args, int);
-                        sprintf(str, "%d", d);
-                        write(1, str, strlen(str));
-                        index += strlen(str);
-                }
-                else if (*format == '%')
-                {
-                        write(1, format, 1);
-                        index++;
-                }
-                }
-                else // format != %
-                {
-                        write(1, format, 1);
-                        index++;
-                }
-                format++;
-        }
-        
-        va_end(args);
-        return (index);
+		else if (*format == 'c')
+		{
+			c = va_arg(args, int);
+			write(1, &c, 1);
+			index++;
+		}
+		else if (*format == 's')
+		{
+			str = va_arg(args, char *);
+			write(1, str, strlen(str));
+			index += strlen(str);
+		}
+		else if (*format == 'd' || *format == 'i')
+		{
+			d = va_arg(args, int);
+			sprintf(str, "%d", d);
+			write(1, str, strlen(str));
+			index += strlen(str);
+		}
+		else if (*format == '%')
+		{
+			write(1, format, 1);
+			index++;
+		}
+		}
+		else
+		{
+			write(1, format, 1);
+			index++;
+		}
+		format++;
+		}
+
+	va_end(args);
+	return (index);
 }
