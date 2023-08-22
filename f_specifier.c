d4573722d71d2e0279ad4d1b9e41fab590f90e66
fatimazaarate <fatima.zaarate@gmail.com>
2023-08-22 13:56:14 +0100

    handling str

--- f_specifier.c
+++ f_specifier.c
@@ -2,12 +2,19 @@
 
 int f_specifier(va_list args, char spec)
 {
+	char *s;
+
 	switch (spec)
 	{
 		case 'c': 
 			return (print_char(va_arg(args, int)));
-		case 's': 
-			return (print_str(va_arg(args, char *)));
+		case 's':
+			s = va_arg(args, char *);
+			if (s == NULL)
+				s = "";
+
+			return (print_str(s));
+
 		case '%': 
 			return (print_char('%'));
 		default:
